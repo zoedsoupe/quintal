163920d05b8ba60b5f40250e4623963b73ed9d21
zoey <zoey.spessanha@zeetech.io>
2026-08-21 13:54:27 -0300

    chore(main): release 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quintal.MixProject do
   def project do
     [
       app: :quintal,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
